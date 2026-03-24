0b793ffc4845254f85c888805ce97c2a2882a6d0
Ilyas Assylbekov <135857875+ilyas-assylbekov@users.noreply.github.com>
2026-03-25 01:02:05 +0500

    main: init the spring boot backend

---
+++ backend/korme-api/src/main/java/com/korme/api/KormeApiApplication.java
@@ -0,0 +1,13 @@
+package com.korme.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KormeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KormeApiApplication.class, args);
+	}
+
+}
---
+++ backend/korme-api/src/test/java/com/korme/api/KormeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korme.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KormeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
